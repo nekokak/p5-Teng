97820b8af7776da256c8afe486fc20478e1aaafd
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-01-08 10:12:54 +0900

    test fix

--- t/lib/DBIx/SkinTest.pm
+++ t/lib/DBIx/SkinTest.pm
@@ -9,9 +9,16 @@ sub prepare_db {
     my $method = "create_$driver";
     my $code   = $class->can( $method );
     if ( ! $code ) {
-        die "$class: Don't know how to create tables for driver $driver";
+        die "$class: Don't know how to create tables for driver '$driver'";
     }
     $code->( $class, $dbh );
+}
+
+# for backward compatibility.
+# remove this method later.
+sub setup_test_db {
+    my $self = shift;
+    $self->prepare_db($self->dbh);
 }
 
 1;
