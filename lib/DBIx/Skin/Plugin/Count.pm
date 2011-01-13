574bcefbb683e12feed9862b7e6bc6fee4f97e4e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-01-13 10:47:54 +0900

    docs

--- lib/DBIx/Skin/Plugin/Count.pm
+++ lib/DBIx/Skin/Plugin/Count.pm
@@ -7,6 +7,7 @@ our @EXPORT = qw/count/;
 
 sub count {
     my ($self, $table, $column, $where) = @_;
+    $column ||= '*';
 
     my $select = $self->sql_builder->new_select();
 
@@ -22,4 +23,40 @@ sub count {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+DBIx::Skin::Plugin::Count - Count rows in database.
+
+=head1 NAME
+
+    package MyDB;
+    use parent qw/DBIx::Skin/;
+    __PACKAGE__->load_plugin('Count');
+
+    package main;
+    my $db = MyDB->new(...);
+    $db->count('user'); # => The number of rows in 'user' table.
+    $db->count('user', '*', {type => 2}); # => SELECT COUNT(*) FROM user WHERE type=2
+
+=head1 DESCRIPTION
+
+This plugin provides shorthand for counting rows in database.
+
+=head1 METHODS
+
+=over 4
+
+=item $db->count($table[, $column[, \%where]]) : Int
+
+I<$table> table name for counting
+
+I<$column> Column name for C<<< COUNT(...) >>>, the default value is '*'.
+
+I<\%where> : HashRef for creating where clause. The format is same as C<< $db->select() >>. This parameter is optional.
+
+I<Return:> The number of rows.
+
+=back
 
