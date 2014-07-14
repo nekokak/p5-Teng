f648494a944960ada97bedda2fa63873f358b328
cho45 <cho45@lowreal.net>
2014-07-14 17:50:10 +0900

    Fix xt tests

--- xt/Utils/postgresql.pm
+++ xt/Utils/postgresql.pm
@@ -2,9 +2,10 @@ package xt::Utils::postgresql;
 
 use strict;
 use warnings;
-use Test::postgresql;
 use Test::More;
 use t::Utils;
+eval "use Test::postgresql";
+plan skip_all => "Test::postgresql required" if $@;
 
 my $pgsql = Test::postgresql->new
     or plan skip_all => $Test::postgresql::errstr;
